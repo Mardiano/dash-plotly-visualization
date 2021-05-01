3c697e4b01a1a596c430a6aa376a2b8ef86aaf9d
Mardiano <13518039@std.stei.itb.ac.id>
2021-05-02 01:24:50 +0700

    Add Graph 2

--- src/components/LineChart.vue
+++ src/components/LineChart.vue
@@ -1,5 +1,6 @@
 <script>
-import { Line } from "vue-chartjs";
+import { Line } from "vue-chartjs"
+import json from '../data/IndikatorUtangLuarNegeri'
 
 export default {
   extends: Line,
@@ -7,18 +8,26 @@ export default {
     this.renderChart(
       {
         labels: [
-          "January",
-          "February",
-          "March",
-          "April",
-          "May",
-          "June",
-          "July"
+          "2007",
+          "2008",
+          "2009",
+          "2010",
+          "2011",
+          "2012",
+          "2013",
+          "2014",
+          "2015",
+          "2016",
+          "2017",
+          "2018",
+          "2019",
+          "2020"
         ],
         datasets: [
           {
-            label: "Data 1",
-            data: [2, 10, 5, 9, 0, 6, 20],
+            label: json[6]["Indikator"],
+            data: [json[6]["2007"], json[6]["2008"], json[6]["2009"], json[6]["2010"], json[6]["2011"], json[6]["2012"], json[6]["2013"], 
+                   json[6]["2014"], json[6]["2015"], json[6]["2016"], json[6]["2017"], json[6]["2018"], json[6]["2019"], json[6]["2020"]],
             backgroundColor: "transparent",
             borderColor: "rgba(1, 116, 188, 0.50)",
             pointBackgroundColor: "rgba(171, 71, 188, 1)"
--- src/components/PieChart.vue
+++ src/components/PieChart.vue
@@ -11,6 +11,7 @@ export default {
       .getContext("2d")
       .createLinearGradient(0, 0, 0, 450);
 
+    
     this.gradient.addColorStop(0, "rgba(255, 0,0, 0.5)");
     this.gradient.addColorStop(0.5, "rgba(255, 0, 0, 0.25)");
     this.gradient.addColorStop(1, "rgba(255, 0, 0, 0)");
