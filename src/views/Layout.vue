700623ff439a114a7329e1e5c7c73774853882c4
Aufa Fadhlurohman <13518009@std.stei.itb.ac.id>
2021-05-02 07:29:44 +0700

    Edit layout

--- src/views/Layout.vue
+++ src/views/Layout.vue
@@ -1,18 +1,31 @@
 <template>
     <div>
         <div class="grid-container">
-            <div class="item1">Visualisasiasisi</div>
-            <div class="item2"></div>
-            <div class="item3">
+            <div class="title">
+                Visualisasiasisi
+            </div>
+
+            <div class="naration">
+
+            </div>
+
+            <div class="mainchart1">
                 <div id="linechart-wrapper">
                 <LineChart id="linechart"/>
                 </div>   
             </div>  
-            <div class="item4"></div>
-            <div class="item5">
+
+            <div class="mainchart2">
+
+            </div>
+
+            <div class="slider">
                 <Slider id="slider" />
             </div>
-            <div class="item6"></div>
+
+            <div class="lainnya">
+                dsdsdds
+            </div>
 
         </div>
     </div>
@@ -33,49 +46,64 @@ export default {
 
 <style lang="css">
 
-.item1 { grid-area: header; }
-.item2 { grid-area: menu; }
-.item3 { grid-area: main; }
-.item4 { grid-area: right; }
-.item5 { grid-area: footer; }
-.item6 { grid-area: footest; }
+.title { 
+    grid-area: title; 
+}
 
+.naration { 
+
+    grid-area: naration; 
+}
+
+.mainchart1 { 
+    grid-area: mainchart1; 
+}
+
+.mainchart2 { 
+    grid-area: mainchart2; 
+}
+
+.slider { 
+    grid-area: slider; 
+}
+
+.lainnya { 
+    grid-area: lainnya; 
+}
 
 .grid-container {
-  display: grid;
-
-  grid-template-columns: 18% 13% 13% 13% 13% 13% 13%;
-  grid-template-rows: 60px 370px 100px auto;
-  grid-template-areas:
-    'menu header header header header header header'
-    'menu main main main right right right'
-    'menu footer footer footer footer footer footer'
-    'menu footest footest footest footest footest footest';
-  grid-gap: 10px;
-  background-color: #c2ecff;
-  padding: 10px;
+    display: grid;
+    grid-template-areas:
+        'naration title title title title title title'
+        'naration mainchart1 mainchart1 mainchart1 mainchart2 mainchart2 mainchart2'
+        'naration slider slider slider slider slider slider'
+        'naration lainnya lainnya lainnya lainnya lainnya lainnya';
+    grid-template-columns: 18% 13% 13% 13% 13% 13% 13%;
+    grid-template-rows: 60px 370px 100px 300px;
+    padding: 10px;
+    grid-gap: 10px;
+    background-color: #c2ecff;
 }
 
 .grid-container > div {
-  background-color: rgba(255, 255, 255, 1.0);
-  text-align: center;
-  font-size: 20px;
-  border-radius: 15px;
-  box-shadow: 0 1px 4px rgba(0, 0, 0, 0.1), 0 0 40px rgba(0, 0, 0, 0.1);
-
+    background-color: rgba(255, 255, 255, 1.0);
+    text-align: center;
+    font-size: 20px;
+    border-radius: 15px;
+    box-shadow: 0 1px 4px rgba(0, 0, 0, 0.1), 0 0 40px rgba(0, 0, 0, 0.1);
 }
 
 #linechart-wrapper {
-  padding: 10px;
-  background-color: rgb(255, 255, 255);
-  width: 520px;
-  margin: 10px;
-  border-radius: 20px;
+    padding: 10px;
+    background-color: rgb(255, 255, 255);
+    width: 520px;
+    margin: 10px;
+    border-radius: 20px;
 }
 
 #linechart {
-  width: 500px;
-  height: 330px;
+    width: 500px;
+    height: 330px;
 }
     
 #slider {
